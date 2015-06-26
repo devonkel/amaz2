1fa21c6e81eb3b7143869eafb63d9fe140469a55
Devon Kelly <devon@Devons-MacBook-Pro.local>
2015-06-26 11:10:05 -0500

    add 1st round of Tab JS

--- js/app.js
+++ js/app.js
@@ -3,11 +3,20 @@ var main = function () {
 
     console.log("hello world!");
 
+    var toDos = [
+        "Finish reading book.",
+        "Take dog to vet.",
+        "Wash car.",
+        "Buy groceries",
+        "Get oil changed"
+    ];
+
     $(".tabs a span").toArray().forEach(function (element) {
         var $element = $(element);
 
         // create a click handler for this element
         $element.on("click", function () {
+            var $element = $(element), $content;
             //make all the tabs inactive
             $(".tabs span").removeClass("active");
             //make the 1st tab active
@@ -17,9 +26,20 @@ var main = function () {
 
             if ($element.parent().is(":nth-child(1)")) {
                 console.log("1st tab clicked!")
+                $content = $("<ul>");
+                var i=0;
+                for (i=toDos.length-1; i>=0; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
+                $("main .content").append($content);
             }
             else if ($element.parent().is(":nth-child(2)")) {
                 console.log("2nd child clicked!");
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+                $("main .content").append($content);
             }
             else if ($element.parent().is(":nth-child(3)")) {
                 console.log("3rd child clicked!");
